744e312f22ab4bac08a79748051d32cb64b84d6e
admin <admin@CHAOS>
2023-02-25 00:46:32 +0700

    Add project files.

---
+++ simple price per area calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter length");
+int length = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter width");
+int width = int.Parse(Console.ReadLine());
+int area = length * width;
+int Par = 15;
+Console.WriteLine("Price per area = " + Par);
+Console.ReadKey();
+Console.WriteLine("You must pay = " + area * Par);
